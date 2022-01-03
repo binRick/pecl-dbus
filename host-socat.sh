47e0fb309da81be430ba7d7ac5e972dc64b50627
root <root@f35.vpnservice.company>
2022-01-03 18:59:51 -0500

    m

--- host-socat.sh
+++ host-socat.sh
@@ -1,1 +1,1 @@
-while :; do socat -d -d -d UNIX-CONNECT:/run/dbus/system_bus_socket TCP:127.0.0.1:49225||sleep 1; sleep .1; done
+while :; do socat -d -d -d -d UNIX-CONNECT:/run/dbus/system_bus_socket TCP:127.0.0.1:49225||sleep 1; sleep .1; done
